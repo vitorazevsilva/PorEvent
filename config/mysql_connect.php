aa1d2496d708330e9b1d5e2bc69762912722145a
Vitor Silva <38570381+vitorazevsilva@users.noreply.github.com>
2021-04-03 15:10:00 +0100

    Update mysql_connect.php

--- config/mysql_connect.php
+++ config/mysql_connect.php
@@ -3,9 +3,9 @@
 
 // variaveis para acesso ao banco de dados
 $DB_SERVER = '127.0.0.1';//Local onde esta instalado o mySQL
-$DB_USERNAME = '260407';//Nome de utilizador a usar no acesso
-$DB_PASSWORD = 'Lt2junIe';//Palavra-chave a usar no acesso
-$DB_NAME = '260407';//Nome da base de dados a aceder
+$DB_USERNAME = '';//Nome de utilizador a usar no acesso
+$DB_PASSWORD = '';//Palavra-chave a usar no acesso
+$DB_NAME = 'porevent';//Nome da base de dados a aceder
 
 // validar a conexao
 $conexao = mysqli_connect($DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DB_NAME );
